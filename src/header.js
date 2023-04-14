fd9467bbaff49a7d2e6fbba8589f88ecf5f4f744
manish <coish5insped@gmail.com>
2023-04-14 11:36:31 -0400

    added nav bar

--- src/header.js
+++ src/header.js
@@ -5,12 +5,23 @@
 
  function tabs () {
     //alert("hello")
+    
     const html = document.getElementById("content")
+    const add = document.createElement('div');
     const btn = document.createElement("button");
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+
 
     btn.textContent = 'menu'
+    btn1.textContent = 'contact'
+    btn2.textContent = 'sub'
 
-     html.append(btn);
+    add.append(btn, btn1, btn2)
+
+    html.append(add);
+   //  html.append(add.append(btn1));
+   //  html.append(add.append(btn2));
 }
 
 export default tabs
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import "./style.css";
 import tabs from "./header.js";
-console.log(tabs())
-const html = document.getElementById("content");
-const pic =
+const pic = 
 "https://imgs.search.brave.com/Tix3xdQoDghjS2mYFv4g8ViLtZfIeYqaPw9dFtl2iHs/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5R/UWZOUTYwTzR4WmhR/aGowaFllWjdnSGFF/OCZwaWQ9QXBp";
+const html = document.getElementById("content");
 
-html.innerHTML = `<h1>welcome to our shop<h1>
-<img src = ${pic}>`;
+tabs()
+
+const mess = document.createElement("h1");
+const img = document.createElement("img");
+img.src = pic;
+mess.textContent = "hello to our establishment";
+html.appendChild(mess);
+html.appendChild(img);
+
+
 
 const p = document.createElement("p");
 const h1 = document.createElement("h1");
